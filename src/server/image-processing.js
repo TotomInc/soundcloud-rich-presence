88f505712058020d36bb3e8bceb9c3b1c43b960b
Thomas Cazade <cazade.thomas@gmail.com>
2019-01-30 08:59:55 +0100

    feat(server): add image-processing utils

---
+++ src/server/image-processing.js
@@ -0,0 +1,16 @@
+const axios = require('axios').default;
+
+/**
+ * Retrieve an image from an URL then transform it into a base64 string.
+ *
+ * @param {string} url url to an image
+ * @returns {Promise<string>} a promise with the image in a base64 string
+ */
+function retrieveImageFromURL(url) {
+  return axios.get(url, { responseType: 'arraybuffer' })
+    .then((response) => Buffer.from(response.data, 'binary').toString('base64'))
+    .then((encoded) => 'data:image/jpeg;base64,'.concat(encoded))
+    .catch((err) => err);
+}
+
+module.exports = { retrieveImageFromURL };
